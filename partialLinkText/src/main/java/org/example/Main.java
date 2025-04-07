5f230fcb87f69a6633ece70ab8f5ef47de9c3de1
Sema Derin <109536999+SemaDerin@users.noreply.github.com>
2025-04-08 00:18:06 +0300

    Partial Link Texts with Selenium

---
+++ partialLinkText/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://demo.guru99.com/test/link.html");
+
+        List<WebElement> elements=  driver.findElements(By.partialLinkText("here"));
+        elements.get(1).click();
+
+
+
+    }
+}
